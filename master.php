a1e1bdc1d1cdc4b004c64ebab450e518706df05a
MarkB-4A5A <474626@edu.rocmn.nl>
2017-04-07 15:25:18 +0200

    Nieuwe link

--- master.php
+++ master.php
@@ -4,6 +4,7 @@
 	<meta name="viewport" content="width=device-width">
 	<title>MA Twente</title>
 	<link rel="stylesheet" type="text/css" href="/css/master.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body>
 <?php
