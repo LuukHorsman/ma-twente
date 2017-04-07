852cd513a6829df0564b98c1529a2e1325d0ad46
MarkB-4A5A <474626@edu.rocmn.nl>
2017-04-07 13:49:56 +0200

    NU MOET HET LUKKEN GVD

--- login/login.php
+++ login/login.php
@@ -1,25 +1,23 @@
-<section class="section">
-	<article class="login">
-		<header class="login_header">
-			<h1>Login to MA Twente</h1>
-		</header>
-		<main class="login_main">
-			<form method="post">
-				<label class="login_label">
-					Username:
-					<input value="<?= (isset($_POST['username']) ? $_POST['username'] : '') ?>" class="login_input" type="text" name="username" id="username" placeholder="Username">
-				</label>
-				<label>
-					Password:
-					<input value="<?= (isset($_POST['password']) ? $_POST['password'] : '') ?>" class="login_input" type="password" name="password" id="password" placeholder="Password">
-				</label>
-				<label>
-					<input type="submit" name="login" value="login">
-				</label>
-			</form>
-		</main>
-		<footer class="login_footer">
-			
-		</footer>
-	</article>
+<section>
+		<article class="login">
+				<header class="login_header">
+					<h1>Inloggen Milieu-Advies Twente</h1>
+				</header>
+				<main class="login_main">
+						<form method="post">
+								<label class="login_label">
+									<input class="login_input" type="text" name="username" id="username" placeholder="E-mail">
+								</label>
+								<label>
+									<input class="login_input" type="password" name="password" id="password" placeholder="Wachtwoord">
+								</label>
+								<label>
+									<input type="submit" name="login" value="Inloggen">
+								</label>
+						</form>
+				</main>
+				<footer class="login_footer">
+
+				</footer>
+		</article>
 </section>
