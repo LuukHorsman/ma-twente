45a113aeb82c71337bafa04863cc9f56da8870dc
MarkB-4A5A <474626@edu.rocmn.nl>
2017-04-11 09:17:58 +0200

    Iets meer up to date dashboard

--- dashboards/files/dashboard.php
+++ dashboards/files/dashboard.php
@@ -1,1 +1,16 @@
-<div></div>
+<section class="dashboard">
+		<header>
+				<div class="nav-trigger"><i class="material-icons">&#xe5d2;</i></div>
+		</header>
+		<nav>
+				<!-- <div>Ingelogd als <?= $_SESSION["voornaam"] . ". " . $_SESSION["achternaam"]; ?></div> -->
+				<ul>
+						<a href="/dashboard"><li>Dashboard</li></a>
+						<a href="/"><li>Incidenten</li></a>
+					  <a href=""><li>Medewerkers</li></a> <!-- ALLEEN VOOR BEHEERDERS -->
+						<a href=""><li>Configuraties</li></a> <!-- ALLEEN VOOR BEHEERDERS -->
+				</ul>
+				<div><a href="/logout">Logout</a></div>
+				<div>Milieu-Advies Twente</div>
+		</nav>
+</section>
