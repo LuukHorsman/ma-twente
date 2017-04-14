c95311ccd46889d2fec593fdc030042a7f22dd8b
MarkB-4A5A <474626@edu.rocmn.nl>
2017-04-14 17:27:06 +0200

    HTML aangepast

--- dashboards/userFiles/createUser.php
+++ dashboards/userFiles/createUser.php
@@ -1,45 +1,65 @@
-<div>
+<div class="user-form">
 	<form method="post">
 
 		<h3>Nieuwe Medewerker Toevoegen</h3>
 
-		Geslacht
-		<select name="geslacht">
-				<?php
-						$geslacht = $login->ReadData(['selectQuery' => 'SELECT `geslacht`.id, `geslacht`.geslacht_naam FROM `geslacht`']);
+		<div class="form-label">Geslacht</div>
+		<div class="form-attribute">
+				<select name="geslacht">
+						<?php
+								$geslacht = $login->ReadData(['selectQuery' => 'SELECT `geslacht`.id, `geslacht`.geslacht_naam FROM `geslacht`']);
 
-						foreach($geslacht as $row) {
-								echo '<option class="" value=' . $row['id'] . '>' . $row['geslacht_naam'] . '</option>';
-						}
-				?>
-		</select><br>
+								foreach($geslacht as $row) {
+										echo '<option class="" value=' . $row['id'] . '>' . $row['geslacht_naam'] . '</option>';
+								}
+						?>
+				</select>
+		</div>
 
-		Voornaam*
-		<input class="<?= (isset($error['match']['voornaam']) ? 'createError' : '') ?>" value="<?= (isset($_POST['voornaam']) ? $_POST['voornaam'] : '') ?>" type="text" name="voornaam"><br>
+		<div class="form-label">Voornaam*</div>
+		<div class="form-attribute">
+				<input class="<?= (isset($error['match']['voornaam']) ? 'createError' : '') ?>" value="<?= (isset($_POST['voornaam']) ? $_POST['voornaam'] : '') ?>" type="text" name="voornaam">
+		</div>
 
-		Tussenvoegsel
-		<input class="<?= (isset($error['match']['tussenvoegsel']) ? 'createError' : '') ?>" value="<?= (isset($_POST['tussenvoegsel']) ? $_POST['tussenvoegsel'] : '') ?>" type="text" name="tussenvoegsel"><br>
+		<div class="form-label">Tussenvoegsel</div>
+		<div class="form-attribute">
+				<input class="<?= (isset($error['match']['tussenvoegsel']) ? 'createError' : '') ?>" value="<?= (isset($_POST['tussenvoegsel']) ? $_POST['tussenvoegsel'] : '') ?>" type="text" name="tussenvoegsel">
+		</div>
 
-		Achternaam*
-		<input class="<?= (isset($error['match']['achternaam']) ? 'createError' : '') ?>" value="<?= (isset($_POST['achternaam']) ? $_POST['achternaam'] : '') ?>" type="text" name="achternaam"><br>
+		<div class="form-label">Achternaam*</div>
+		<div class="form-attribute">
+				<input class="<?= (isset($error['match']['achternaam']) ? 'createError' : '') ?>" value="<?= (isset($_POST['achternaam']) ? $_POST['achternaam'] : '') ?>" type="text" name="achternaam">
+		</div>
 
-		Intern telefoonnummer*
-		<input class="<?= (isset($error['match']['inter_telefoonnummer']) ? 'createError' : '') ?>" value="<?= (isset($_POST['inter_telefoonnummer']) ? $_POST['inter_telefoonnummer'] : '') ?>" type="text" name="inter_telefoonnummer"><br>
+		<div class="form-label">Intern telefoonnummer*</div>
+		<div class="form-attribute">
+				<input class="<?= (isset($error['match']['inter_telefoonnummer']) ? 'createError' : '') ?>" value="<?= (isset($_POST['inter_telefoonnummer']) ? $_POST['inter_telefoonnummer'] : '') ?>" type="text" name="inter_telefoonnummer">
+		</div>
 
-		Afdeling*
-		<select class="<?= (isset($error['match']['afdeling_id']) ? 'createError' : '') ?>" name="afdeling_id">
-				<option value="0">Selecteer een afdeling</option>
-				<?php
-						$afdeling = $login->ReadData(['selectQuery' => 'SELECT `afdeling`.id, `afdeling`.afdeling_naam FROM `afdeling`']);
+		<div class="form-label">Afdeling*</div>
+		<div class="form-attribute">
+				<select class="<?= (isset($error['match']['afdeling_id']) ? 'createError' : '') ?>" name="afdeling_id">
+						<option value="0">Selecteer een afdeling</option>
+						<?php
+								$afdeling = $login->ReadData(['selectQuery' => 'SELECT `afdeling`.id, `afdeling`.afdeling_naam FROM `afdeling`']);
 
-						foreach($afdeling as $row) {
-								echo '<option ' . (isset($_POST['afdeling_id']) && $_POST['afdeling_id'] == $row['id'] ? 'selected' : '') . ' value=' . $row['id'] . '>' . $row['afdeling_naam'] . '</option>';
-						}
-				?>
-		</select><br><br>
+								foreach($afdeling as $row) {
+										echo '<option ' . (isset($_POST['afdeling_id']) && $_POST['afdeling_id'] == $row['id'] ? 'selected' : '') . ' value=' . $row['id'] . '>' . $row['afdeling_naam'] . '</option>';
+								}
+						?>
+				</select>
+		</div>
 
-		Email* <input class="<?= (isset($error['match']['email']) ? 'createError' : '') ?>" value="<?= (isset($_POST['email']) ? $_POST['email'] : '') ?>" type="text" name="email"><br>
-		Wachtwoord* <input class="<?= (isset($error['match']['wachtwoord']) ? 'createError' : '') ?>" value="<?= (isset($_POST['wachtwoord']) ? $_POST['wachtwoord'] : '') ?>" type="password" name="wachtwoord"><br>
+		<div class="form-label">Email*</div>
+		<div class="form-attribute">
+				<input class="<?= (isset($error['match']['email']) ? 'createError' : '') ?>" value="<?= (isset($_POST['email']) ? $_POST['email'] : '') ?>" type="text" name="email">
+		</div>
+
+		<div class="form-label">Wachtwoord*</div>
+		<div class="form-attribute">
+				<input class="<?= (isset($error['match']['wachtwoord']) ? 'createError' : '') ?>" value="<?= (isset($_POST['wachtwoord']) ? $_POST['wachtwoord'] : '') ?>" type="password" name="wachtwoord">
+		</div>
+
 		<input type="radio" name="status_id" value="1" checked>Maak gebruiker een administrator<br><br>
 		<input type="submit" name="subbutton" value="Gebruiker Toevoegen">
 	</form>
