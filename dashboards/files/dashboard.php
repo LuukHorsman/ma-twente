10bb47861ed0cab563519bf466a74288bdcb55d1
MarkB-4A5A <474626@edu.rocmn.nl>
2017-04-14 22:01:29 +0200

    Testing hopelijk werkt het

--- dashboards/files/dashboard.php
+++ dashboards/files/dashboard.php
@@ -1,20 +1,9 @@
 <section class="dashboard">
-		<!-- <header>
-				<div class="nav-trigger"><i class="material-icons">&#xe5d2;</i></div>
+		<header>
+				<div>Ingelogd als <?= $_SESSION["voornaam"] . ". " . $_SESSION["achternaam"]; ?></div>
+				<div><a href="/logout">Logout</a></div>
 		</header>
 		<nav>
-				<ul>
-						<a href="/dashboard"><li>Dashboard</li></a>
-						<a href="/"><li>Incidenten</li></a>
-					  <a href=""><li>Medewerkers</li></a>
-						<a href=""><li>Configuraties</li></a>
-				</ul>
-				<div><a href="/logout">Logout</a></div>
-				<div>Milieu-Advies Twente</div>
-		</nav> -->
-		<section class="content">
-			<?php
-				include($user_page);
-			?>
-		</section>
+				Navigatie
+		</nav>
 </section>
--- dashboards/userFiles/dashboard.php
+++
@@ -1,9 +0,0 @@
-<section class="dashboard">
-		<header>
-				<div>Ingelogd als <?= $_SESSION["voornaam"] . ". " . $_SESSION["achternaam"]; ?></div>
-				<div><a href="/logout">Logout</a></div>
-		</header>
-		<nav>
-				Navigatie
-		</nav>
-</section>
